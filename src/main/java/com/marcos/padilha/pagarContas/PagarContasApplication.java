18a03bba7721b7dbd3156dde28455e7849188d9b
tonhodofus <marcospad92@gmail.com>
2020-10-10 19:52:21 -0300

    - Commit inicial

---
+++ src/main/java/com/marcos/padilha/pagarContas/PagarContasApplication.java
@@ -0,0 +1,13 @@
+package com.marcos.padilha.pagarContas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PagarContasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PagarContasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/marcos/padilha/pagarContas/PagarContasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marcos.padilha.pagarContas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PagarContasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
